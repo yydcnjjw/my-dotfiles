90a16964928d973a036853d7a965761e4639098e
jiangjw <jiangjw@koal.com>
2021-08-20 18:13:45 +0800

    update

---
+++ run_once_install-packages.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+sudo pacmsn -S --needed --noconfirm i3-gaps i3lock dmenu picom dunst alacritty tmux fcitx5 fcitx5-rime fcitx5-mozc fcitx5-qt fcitx5-gtk
+
+# TODO: polybar(aur)
