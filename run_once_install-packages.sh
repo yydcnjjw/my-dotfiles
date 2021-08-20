ac36347579aa0c521f3a5e55ebfebbbf2ebad7c5
jiangjw <jiangjw@koal.com>
2021-08-20 18:20:13 +0800

    update

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -4,4 +4,9 @@ IFS=$'\n\t'
 
 sudo pacman -S --needed --noconfirm i3-gaps i3lock dmenu picom dunst alacritty tmux fcitx5 fcitx5-rime fcitx5-mozc fcitx5-qt fcitx5-gtk
 
+if [ ! -d $HOME/.oh-my-zsh/ ]
+then
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
 # TODO: polybar(aur)
