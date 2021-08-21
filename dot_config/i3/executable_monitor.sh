85e7e92aad99b3fee5b616e6827403a80608cbbb
yydcnjjw <yydcnjjw@gmail.com>
2021-08-21 11:41:22 +0800

    update

--- dot_config/i3/executable_monitor.sh
+++ dot_config/i3/executable_monitor.sh
@@ -2,8 +2,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-prev=$(xdpyinfo | awk '/dimensions/{print $2}')
+echo $(xdpyinfo | awk '/dimensions/{print $2}') > /tmp/.monitor_watch
+
 while true; do
+    prv=$(cat /tmp/.monitor_watch)
     cur=$(xdpyinfo | awk '/dimensions/{print $2}')
     if [ $cur != $prev ]
     then
